7b7ac3f291324a4d956c10c64aea517dada13f67
thodson <thodson@usgs.gov>
2023-03-12 23:15:06 -0500

    Not working well, normal prior helps

--- ratingcurve/ratingmodel.py
+++ ratingcurve/ratingmodel.py
@@ -191,8 +191,10 @@ class PowerLawRating(Rating, PowerLawPlotMixin):
         hs1 = np.empty((self.segments + 1, 1))
         hs1[:-1] = hs.eval()
         hs1[-1] = np.inf
-
-        b = pm.Deterministic('b', at.log( at.clip(h - hs, 0, hs1[1:] - hs) + ho)) # best yet
+        x = at.clip(h - hs, 1e-6, np.inf)
+        #b = pm.Deterministic('b', at.clip(at.log(x), at.log(ho), at.log(hs1[1:] - hs))) # works for 2 segment
+        b = pm.Deterministic('b', at.clip(at.log(x), at.log(ho), at.log(hs1[1:] - hs + ho))) # works for 2 segment
+        #b = pm.Deterministic('b', at.log( at.clip(h - hs + ho, 0, hs1[1:] - hs + ho)))
         sigma = pm.HalfCauchy("sigma", beta=0.1)
         mu = pm.Normal("mu", a + at.dot(w, b), sigma + self.q_sigma, observed=self.y)
 
