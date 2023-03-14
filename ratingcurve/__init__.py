8d29ee02c355456d3087a92f7feb7d54bddb84de
thodson-usgs <thodson-usgs@users.noreply.github.com>
2023-03-14 21:10:19 +0000

    deploy: 2efb7e5cc222aa7bcd8cda15c9c0d9835d098ba9

--- ratingcurve/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("ratingcurve")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = "dev"
