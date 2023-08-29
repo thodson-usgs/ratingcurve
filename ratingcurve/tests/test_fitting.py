66bf9a57c3cfd9b7a93cf7556a59b865172febef
Timothy Hodson <34148978+thodson-usgs@users.noreply.github.com>
2023-08-28 22:45:21 -0500

    Update test_fitting.py
    NUTS is too slow to test the full functionality.

--- ratingcurve/tests/test_fitting.py
+++ ratingcurve/tests/test_fitting.py
@@ -3,10 +3,26 @@ import pytest
 from ..ratingmodel import PowerLawRating, SplineRating
 from .. import data
 
+def test_nuts_fit():
+    """NUTS is slow, so test a limited functionality"""
+    df = data.load('green channel')
+
+    rating = PowerLawRating(q=df['q'],
+                            h=df['stage'],
+                            q_sigma=df['q_sigma'],
+                            segments=2)
+
+    trace = rating.fit(method=method)
+    df_model = rating.table(trace)
+
+    assert len(df_model) > 0
+    assert all(df_model.stage >= 0)
+    assert all(df_model.discharge >= 0)
+
 @pytest.mark.parametrize('ratingmodel', ['powerlaw', 'spline'])
-@pytest.mark.parametrize('method', ['advi', 'nuts'])
 @pytest.mark.parametrize('segments, dof', [(1, 3), (4, 8)])
-def test_rating_fit(ratingmodel, method, segments, dof):
+
+def test_advi_fit(ratingmodel, segments, dof):
     df = data.load('green channel')
 
     if ratingmodel == 'powerlaw':
@@ -20,7 +36,7 @@ def test_rating_fit(ratingmodel, method, segments, dof):
                               q_sigma=df['q_sigma'],
                               df=dof)
 
-    trace = rating.fit(method=method)
+    trace = rating.fit(method='advi')
     df_model = rating.table(trace)
 
     assert len(df_model) > 0
