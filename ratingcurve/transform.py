2b6c65ae286af17a77748e0d4625ebd9160a1d98
Keith Doore <kjdoore@gmail.com>
2023-09-15 09:01:42 -0500

    Updated docstring for consistency

--- ratingcurve/transform.py
+++ ratingcurve/transform.py
@@ -11,18 +11,22 @@ if TYPE_CHECKING:
 
 
 class Transform:
-    """Transformation class
-
-    All children of Transform must have transfom and untransform methods
     """
+    Transformation class.
+
+    All children of Transform must have transfom and untransform methods.
+    """
+    
     def __init__(self, x):
-        """Create empty Transform object
+        """
+        Create empty Transform object.
         """
         self.mean_ = None
         self.std_ = None
 
     def transform(self, x: ArrayLike) -> ArrayLike:
-        """Transform data
+        """
+        Transform data.
 
         Parameters
         ----------
@@ -31,116 +35,138 @@ class Transform:
 
         Returns
         -------
-        ArrayLike
+        transformed_x : array_like
             Transformed data.
         """
         return x
 
     def untransform(self, x: ArrayLike) -> ArrayLike:
+        """
+        Undo data transform (untransform).
+
+        Parameters
+        ----------
+        transformed_x : array_like
+            Transformed data.
+
+        Returns
+        -------
+        x: array_like
+            Untransformed data.
+        """
         return x
 
 
 class ZTransform(Transform):
-    """Z-transforms data to have zero mean and unit variance
+    """
+    Z-transforms data to z-score scale (i.e., zero mean and unit variance).
     """
 
     def __init__(self, x: ArrayLike):
-        """Create a ZTransform object
+        """
+        Create a ZTransform object.
 
         Parameters
         ----------
         x : array_like
-          Data that defines the transform.
+            Data that defines the transform.
         """
         self.mean_ = np.nanmean(x, axis=0)
         self.std_ = np.nanstd(x, axis=0)
 
     def transform(self, x: ArrayLike) -> ArrayLike:
-        """Transform to z score (standardize x)
+        """
+        Transform to z-score scale (standardize x).
 
         Parameters
         ----------
         x : array_like
-          Data to be transformed.
+            Data to be transformed.
 
         Returns
         -------
-        ArrayLike
-            original data standardized to zero mean and unit variance (z-score)
+        transformed_x : array_like
+            Data standardized to zero mean and unit variance (z-score).
         """
         return (x - self.mean_) / self.std_
 
     def untransform(self, z: ArrayLike) -> ArrayLike:
-        """Transform from z score back to original units.
+        """
+        Transform from z-score scale back to original units.
 
         Parameters
         ----------
-        z : array_like
-          Transformed data
+        transformed_x : array_like
+            Z-Transformed data.
 
         Returns
         -------
-        ArrayLike
-            z-scores transformed back to original units.
+        x : array_like
+            Untransformed data back in original units.
         """
         return z * self.std_ + self.mean_
 
 
 class LogZTransform(ZTransform):
-    """Log transform then takes z-score.
+    """
+    Z-transforms logarithm of data to z-score scale (i.e., log of data has 
+    zero mean and unit variance).
     """
 
     def __init__(self, x: ArrayLike):
-        """Create a LogZTransform for x.
+        """
+        Create a LogZTransform object.
 
         Parameters
         ----------
         x : array_like
-          Data that defines the transform.
+            Data that defines the transform.
         """
         log_x = np.log(x)
         super().__init__(log_x)
 
     def transform(self, x: ArrayLike) -> ArrayLike:
-        """Transform to log z-score
-
-        Logs the data then standardizes to zero mean and unit variance.
+        """
+        Transform to log z-score scale (standardize log(x)).
 
         Parameters
         ----------
         x : array_like
-          Data to be transformed.
+            Data to be transformed.
 
         Returns
         -------
-        ArrayLike
-            log transform data standardized to zero mean and unit variance (log z-score)
+        transformed_x : array_like
+            Log transformed data standardized to zero mean and unit variance (log z-score).
         """
         log_x = np.log(x)
         return super().transform(log_x)
 
     def untransform(self, z: ArrayLike) -> ArrayLike:
-        """Reverse log z-score transformation.
+        """
+        Transform from log z-score scale back to original units.
 
         Parameters
         ----------
-        z : array_like
-          Transformed data.
+        transformed_x : array_like
+            LogZTransformed data.
 
         Returns
         -------
-        ArrayLike
-            log z-scores transformed back to original units.
+        x : array_like
+            Untransformed data back in original units.
         """
         log_x = super().untransform(z)
         return np.exp(log_x)
 
 
 class UnitTransform(Transform):
-    """Transforms data to the unit (0 to 1) interval.
+    """
+    Transforms data to the unit (0 to 1) interval.
     """
     def __init__(self, x: ArrayLike):
-        """Create UnitTransform of array
+        """
+        Create a UnitTransform object.
 
         Parameters
         ----------
@@ -150,7 +176,8 @@ class UnitTransform(Transform):
         self.max_ = np.nanmax(x, axis=0)
 
     def transform(self, x: ArrayLike) -> ArrayLike:
-        """Transform to unit interval
+        """
+        Transform to unit interval.
 
         Parameters
         ----------
@@ -159,32 +186,35 @@ class UnitTransform(Transform):
         
         Returns
         -------
-        ArrayLike
-            Original data transformed to unit interval
+        transformed_x : array_like
+            Transformed data to unit interval (0 to 1).
         """
         return x/self.max_
 
     def untransform(self, x: ArrayLike) -> ArrayLike:
-        """Transform from unit interval back to original units
+        """
+        Transform from unit interval back to original units.
 
         Parameters
         ----------
-        x : array_like
-          Transformed data.
+        transformed_x : array_like
+            UnitTransformed data.
 
         Returns
         -------
-        ArrayLike
-            Unit interval transformed back to original units.
+        x: array_like
+            Untransformed data back in original units.
         """
         return x*self.max_
 
 
 class Dmatrix():
-    """Transform for spline design matrix
     """
-    def __init__(self, stage: ArrayLike, df: int, form: str = 'cr') -> None:
-        """Create a Dmatrix object
+    Transform for spline design matrix.
+    """
+    def __init__(self, x: ArrayLike, df: int, form: str = 'cr') -> None:
+        """
+        Create a design matrix (Dmatrix) object.
 
         Create a design matrix for a natural cubic spline, which is a cubic
         spline that is additionally constrained to be linear at the boundaries.
@@ -193,48 +223,52 @@ class Dmatrix():
         
         Parameters
         ----------
-        stage : array_like
-          Stage data
+        x : array_like
+            Data that defines the transform.
         df : int
-          Degrees of freedom
+            Degrees of freedom.
         form : str
-          Spline form
+            Spline form.
 
         """
         n_knots = df - 1
-        self.knots = compute_knots(stage.min(), stage.max(), n=n_knots)
-        temp = dmatrix(f"{form}(stage, df={df}) - 1", {"stage": stage})
+        self.knots = compute_knots(x.min(), x.max(), n=n_knots)
+        temp = dmatrix(f"{form}(x, df={df}) - 1", {"x": x})
         self.design_info = temp.design_info
 
-    def transform(self, stage: ArrayLike) -> ArrayLike:
-        """Transform stage using spline design matrix
+    def transform(self, x: ArrayLike) -> ArrayLike:
+        """
+        Transform data using spline design matrix.
  
         Parameters
         ----------
-        stage : array-like
-          Stage data
+        x : array-like
+            Data to be transformed.
 
         Returns
         -------
-        ArrayLike
-            Transformed data
+        transformed_x : array_like
+            Transformed data into spline design matrix.
         """
-        return np.asarray(build_design_matrices([self.design_info], {"stage": stage})).squeeze()
+        return np.asarray(build_design_matrices([self.design_info], {"x": x})).squeeze()
 
 
 def compute_knots(minimum: float, maximum: float, n: int) -> ArrayLike:
-    """Return list of spline knots
+    """
+    Return list of spline knots.
 
     Parameters
     ----------
-    minimum, maximum : float
-        Minimum and maximum stage (h) observations.
+    minimum: float
+        Minimum data value to consider.
+    maximum : float
+        Maximum data value to consider.
     n : int
         Number of knots.
 
     Returns
     -------
-    ArrayLike
+    knots : array_like
         List of spline knots.
     """
     return np.linspace(minimum, maximum, n)
