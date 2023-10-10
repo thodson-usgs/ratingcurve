aa6e1ec8c6f71a6a874385fd9a8fcf7dab4a0ae2
Keith Doore <kjdoore@gmail.com>
2023-10-10 12:22:45 -0500

    Updated unit tests

--- ratingcurve/tests/test_fitting.py
+++ ratingcurve/tests/test_fitting.py
@@ -1,68 +1,67 @@
 import pytest
 import numpy as np
 
-from ..ratingmodel import PowerLawRating, SplineRating
+from ..ratings import PowerLawRating, SplineRating
 from .. import data
 
 def test_nuts_fit():
-    """Test fitting a power law by NUTS
+    """
+    Test fitting a power law by NUTS.
 
-    NUTS is slow, so test a limited functionality"""
+    NUTS is slow, so test a limited functionality.
+    """
 
     df = data.load('green channel')
 
-    rating = PowerLawRating(q=df['q'],
-                            h=df['stage'],
-                            q_sigma=df['q_sigma'],
-                            segments=2)
+    rating = PowerLawRating(method='nuts')
 
-    trace = rating.fit(method='nuts')
-    df_model = rating.table(trace)
+    idata = rating.fit(df['stage'], df['q'], q_sigma=df['q_sigma'])
+    df_model = rating.table()
 
     assert len(df_model) > 0
     assert all(df_model.stage >= 0)
     assert all(df_model.discharge >= 0)
 
+
 @pytest.mark.parametrize('ratingmodel', ['powerlaw', 'spline'])
 @pytest.mark.parametrize('segments, dof', [(1, 3), (4, 8)])
-
 def test_advi_fit(ratingmodel, segments, dof):
-    """Test fitting a power law by ADVI
+    """
+    Test fitting a power law by ADVI.
     """
     df = data.load('green channel')
 
     if ratingmodel == 'powerlaw':
-        rating = PowerLawRating(q=df['q'],
-                                h=df['stage'],
-                                q_sigma=df['q_sigma'],
-                                segments=segments)
+        model_config = {'segments': segments,
+                        'prior' : {'distribution': 'uniform'}}
+        rating = PowerLawRating(model_config=model_config, method='advi')
     elif ratingmodel == 'spline':
-        rating = SplineRating(q=df['q'],
-                              h=df['stage'],
-                              q_sigma=df['q_sigma'],
-                              df=dof)
+        model_config = {'mean': 0, 'ds': 1, 'df': dof}
+        rating = SplineRating(model_config=model_config, method='advi')
 
-    trace = rating.fit(method='advi')
-    df_model = rating.table(trace)
+    idata = rating.fit(df['stage'], df['q'], q_sigma=df['q_sigma'])
+    df_model = rating.table()
 
     assert len(df_model) > 0
     assert all(df_model.stage >= 0)
     assert all(df_model.discharge >= 0)
 
+
 def test_no_zero_flows():
-    """Test that a zero flow raises an error
+    """
+    Test that a zero flow raises an error.
     """
     q = np.array([0, 1, 2])
     h = np.array([0, 1, 2])
 
     with pytest.raises(ValueError):
-        rating = PowerLawRating(q=q,
-                                h=h,
-                                segments=1)
+        rating = PowerLawRating()
+        idata = rating.fit(h, q)
 
 
 def test_zero_flow_prior():
-    """Test the zero-flow prior
+    """
+    Test the zero-flow prior.
 
     The first breakpoint should be below the lowest observed flow.
     """
@@ -71,8 +70,7 @@ def test_zero_flow_prior():
     q_min = df['q'].min()
     
     with pytest.raises(ValueError):
-        rating = PowerLawRating(q=df['q'],
-                                h=df['stage'],
-                                segments=1,
-                                prior = {'distribution': 'normal', 'mu': [q_min], 'sigma': [1]})
-
+        model_config = {'segments': 1,
+                        'prior' : {'distribution': 'normal', 'mu': [q_min], 'sigma': [1]}}
+        rating = PowerLawRating(model_config=model_config)
+        idata = rating.fit(df['stage'], df['q'])
